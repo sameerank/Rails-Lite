af53c39e2b98f73e770b2309af818c40a16a707e
sameerank <skunche@gmail.com>
2016-02-02 17:15:43 -0800

    route matches regular expressions

--- lib/router.rb
+++ lib/router.rb
@@ -2,10 +2,16 @@ class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern = pattern
+    @http_method = http_method
+    @controller_class = controller_class
+    @action_name = action_name
   end
 
   # checks if pattern matches path and method matches request method
   def matches?(req)
+    (@pattern =~ req.path) &&
+    (@http_method.to_s == req.request_method.downcase)
   end
 
   # use pattern to pull out route params (save for later?)
@@ -18,10 +24,12 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
   # evaluate the proc in the context of the instance
@@ -32,6 +40,9 @@ class Router
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
   end
 
   # should return the route that matches this request
